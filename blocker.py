cd9acaa833d376fa7818d85b38c858f31e0d8f2e
Luis <luisfaridta@gmail.com>
2018-08-02 21:02:57 +0100

    app can modify hosts file (copy on folder)

--- blocker.py
+++ blocker.py
@@ -9,6 +9,20 @@ website_list = ["www.facebook.com", "facebook.com"]
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
         print("Working hours")
+        with open(temp_path, "r+") as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + " " + website+"\n")
     else:
+        with open(temp_path, "r+") as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
         print("Fun time")
     time.sleep(5)
